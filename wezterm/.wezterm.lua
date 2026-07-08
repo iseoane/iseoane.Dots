af7e17e3adbc2f030fa2e1234e826736ee345e17
iseoane <iseoane@quental.com>
2026-07-08 09:12:57 +0200

    feat(wezterm): maximize window on startup

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -33,4 +33,9 @@ config.mouse_bindings = {
   },
 }
 
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 return config
